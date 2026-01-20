8a6b346a5725c341aa0aba49bbba5c566659a820
upohaar <sumaiyanobeupohaar@gmail.com>
2026-01-20 22:56:33 +0600

    checking

--- add new child in the li.js
+++ add new child in the li.js
@@ -5,10 +5,10 @@ for ( const frutisList of frutisLists){
 document.getElementById('btn-input').addEventListener('click', function(){
     const input = document.getElementById('input-text')
     const inputText = input.value;
-
     const inputBox = document.getElementById('fruits-list');
     const p = document.createElement('li');
     p.innerText = inputText;
     inputBox.appendChild(p)
     input.value='';
+    
 })
